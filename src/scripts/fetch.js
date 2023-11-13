6fb8c4ed6988bb26cc5f449efa5cbf1b3ecc4aa4
arya <you@example.com>
2023-11-13 19:22:14 +0100

    fetch and news js updated

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,10 +1,33 @@
 export default (function () {
-  if (!window.location.pathname.includes("index.html")) return // guard clause
+  if (!window.location.pathname.includes("index.html")) return; // guard clause
 
-  const data = fetch(`https://api.nytimes.com/svc/topstories/v2/{movies}.json`
-  )
-  console.log(data)
+        
+        // Your API key
+        const apiKey = 'R6JswxR2AtoZ4AIEMyGPr0Z4oGATvNsL';
 
+        // Function to fetch articles
+        async function fetchNYTHealthArticles() {
+            try {
+                const response = await fetch('https://api.nytimes.com/svc/topstories/v2/health.json?api-key=' + apiKey);
+                const data = await response.json();
+                
+                // Update the content in HTML
+                const articles = data.results.slice(0, 5); // Fetching the first 5 articles
+                const articleContent = articles.map(article => `
+                    <div>
+                        <h3>${article.title}</h3>
+                        <p>${article.abstract}</p>
+                        <a href="${article.url}" target="_blank">Read more</a>
+                    </div>
+                `).join('');
 
+                document.querySelector('.newsArticleText').innerHTML = articleContent;
+            } catch (error) {
+                console.error('Error fetching articles:', error);
+                document.querySelector('.newsArticleText').innerText = 'Failed to fetch articles.';
+            }
+        }
 
-})()
+        // Call the function to fetch articles
+        fetchNYTHealthArticles();
+      })();
--- src/scripts/news.js
+++ src/scripts/news.js
@@ -1,16 +1,27 @@
 export default (function () {
-    if (!window.location.pathname.includes("index.html")) return // guard clause
+  if (!window.location.pathname.includes("index.html")) return; // guard clause
 
-    const DROPDOWN = document.querySelector("#dropDown")
+  const DROPDOWN = document.querySelector("#dropDown");
+  DROPDOWN.addEventListener("click", clickHandler);
 
-    DROPDOWN.addEventListener("click", clickHandler)
 
-    function clickHandler() {
-        var x = document.querySelector(".newsArticle");
-        if (x.style.display === "none") {
-          x.style.display = "grid";
-        } else {
-          x.style.display = "none";
-        }
+  function clickHandler() {
+    var x = document.querySelector(".newsArticle");
+    var arrow = document.getElementById("dropDown");
+
+    // Toggle visibility of the article
+    x.classList.toggle("hidden");
+
+    // Toggle arrow icon based on the visibility of .newsArticle
+    if (x.classList.contains("hidden")) {
+      arrow.classList.remove("fa-chevron-down");
+      arrow.classList.add("fa-chevron-right");
+    } else {
+      arrow.classList.remove("fa-chevron-right");
+      arrow.classList.add("fa-chevron-down");
     }
-})()
+  }
+
+
+
+})();
