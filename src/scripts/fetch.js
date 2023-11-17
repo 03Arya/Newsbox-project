138fa2cb0e8154a7aa974f23e581d097f3ac5e8b
arya <you@example.com>
2023-11-17 20:06:26 +0100

    working on archives and stuff

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,4 +1,4 @@
-export default (function () {
+(function () {
     if (!window.location.pathname.includes("index.html")) return; // guard clause
 
     const apiKey = 'R6JswxR2AtoZ4AIEMyGPr0Z4oGATvNsL';
@@ -9,22 +9,56 @@ export default (function () {
             const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${endpoint}.json?api-key=${apiKey}`);
             const data = await response.json();
 
-            const articles = data.results.slice(1, 6);
-            const articleContent = articles.map(article => `
-              <a href="${article.url}" target="_blank">
-                  <div class="articleContainer">
-                      <h3 class="articleHeader">${article.title}</h3>
-                      <p class="articleText">${article.abstract}</p>
-                  </div>
-              </a>
-          `).join('');
+            if (response.ok) {
+                const articles = data.results.slice(1, 6);
+                const articleContent = articles.map((article, index) => `
+                <a href="${article.url}" target="_blank" class="swipe-article" data-index="${index}">
+                    <div class="articleContainer">
+                        <h3 class="articleHeader">${article.title}</h3>
+                        <p class="articleText">${article.abstract}</p>
+                    </div>
+                </a>
+            `).join('');
 
-            document.querySelector(containerSelector).innerHTML = articleContent;
+                document.querySelector(containerSelector).innerHTML = articleContent;
+
+                // Add swipe event listeners to each article container
+                const swipeArticles = document.querySelectorAll('.swipe-article');
+                swipeArticles.forEach(article => {
+                    let startX;
+
+                    article.addEventListener('touchstart', (e) => {
+                        startX = e.touches[0].clientX;
+                    });
+
+                    article.addEventListener('touchend', (e) => {
+                        const distX = e.changedTouches[0].clientX - startX;
+
+                        // Adjust the threshold based on your needs
+                        if (distX > 50) {
+                            // Swipe right, save to local storage
+                            const articleIndex = article.getAttribute('data-index');
+                            const savedArticles = JSON.parse(localStorage.getItem('savedArticles')) || [];
+
+                            // Check if the article is not already in the saved list
+                            if (!savedArticles.includes(articleIndex)) {
+                                savedArticles.push(articleIndex);
+                                localStorage.setItem('savedArticles', JSON.stringify(savedArticles));
+                                alert('Article saved to local storage!');
+                            }
+                        }
+                    });
+                });
+            } else {
+                console.error(`Error fetching articles for ${endpoint}: ${data.message}`);
+                document.querySelector(containerSelector).innerText = 'Failed to fetch articles.';
+            }
         } catch (error) {
             console.error('Error fetching articles:', error);
             document.querySelector(containerSelector).innerText = 'Failed to fetch articles.';
         }
     }
+
 
     // Fetch articles for Business
     fetchArticles('business', '.ArticleTextBusiness', '#businessDropdown', 'ArticleBusiness');
